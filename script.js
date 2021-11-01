0d2c89299eabbfd7554922c2598b423383266892
soto-manuel <manussoto69@gmail.com>
2021-11-01 16:30:52 -0300

    desafio entregable clase 1

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
-
 console.log("INICIO");
-
-// VER PRIMER CLASE 1 TODAVIA !!!!!
 
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
-console.log (nombre + " " + apellido);
-alert (nombre +" "+ apellido);
 
-// CLASE 2 
+console.log ("Hola, " +nombre+ " " +apellido);
 
-// let edadPromedio = 30;
-// let edadUsuario = prompt("ingrese su edad")
-// let edadUsuarioMenorPromedio = edadUsuario < edadPromedio
+let saludo = prompt("Como has estado? " +nombre+" "+ apellido);
+console.log(saludo);
 
-// if (edadUsuarioMenorPromedio) {
-//     console.log("Edad menor al promedio")
-// }
-// else{
-//     console.log("edad valida")
-// }
+let suma =  parseInt(prompt("Asi que " +saludo+ ". Ahora, diganos su edad"));
+alert (suma + ". Por ultimo le vamos a sumar 21");
 
-// let usuario = prompt("escriba su nombre: ");
-// let coderHouse = "CODERHOUSE";
+let resultado = parseInt( suma + 21 )
 
-// if(usuario != coderHouse){
-//     console.log("rechazado");
-// } else{
-//     console.log("bienvenido");
-// }
-
-
-
-/* Pedir nombre y fijarse que no este vacio
-Pedir edad como entrada usando el nombre en el mensaje "Hola <nombre>, cual es tu edad?"
-Y evaluar si es o no mayor de 30 años e imprimir un mensaje acorde. */
-
-
-// let nombreUsuario = prompt ("indique su nombre")
-
-// console.log("nombreUsuario");
-
-// INCOMPLETO 
+console.log(resultado);
+alert(resultado);
 
 console.log("FIN");
