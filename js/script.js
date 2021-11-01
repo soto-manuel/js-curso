2cc6332d9ba8e005a77a1b9f4d86e2f823e63505
soto-manuel <manussoto69@gmail.com>
2021-11-01 14:29:44 -0300

    comienzo

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+console.log("INICIO");
+
+// VER PRIMER CLASE 1 TODAVIA !!!!!
+
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+console.log (nombre + " " + apellido);
+alert (nombre +" "+ apellido);
+
+// CLASE 2 
+
+// let edadPromedio = 30;
+// let edadUsuario = prompt("ingrese su edad")
+// let edadUsuarioMenorPromedio = edadUsuario < edadPromedio
+
+// if (edadUsuarioMenorPromedio) {
+//     console.log("Edad menor al promedio")
+// }
+// else{
+//     console.log("edad valida")
+// }
+
+// let usuario = prompt("escriba su nombre: ");
+// let coderHouse = "CODERHOUSE";
+
+// if(usuario != coderHouse){
+//     console.log("rechazado");
+// } else{
+//     console.log("bienvenido");
+// }
+
+
+
+/* Pedir nombre y fijarse que no este vacio
+Pedir edad como entrada usando el nombre en el mensaje "Hola <nombre>, cual es tu edad?"
+Y evaluar si es o no mayor de 30 años e imprimir un mensaje acorde. */
+
+
+// let nombreUsuario = prompt ("indique su nombre")
+
+// console.log("nombreUsuario");
+
+// INCOMPLETO 
+
+console.log("FIN");
