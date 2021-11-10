df43f923a7a2f847b0fa679e285d871fc6f7884a
soto-manuel <manussoto69@gmail.com>
2021-11-09 21:05:52 -0300

    entrega desafío 3

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 console.log("INICIO");
 
-let usuarioIngresado = prompt("Ingrese su nombre: ");
 
 
-if (((usuarioIngresado === "Cami") || usuarioIngresado === "CAMI") || usuarioIngresado === "cami") {
-    alert("Buenos dias, tardes o noche, Cami, CAMI o cami :)");
-    let edadIngresada = prompt("ingrese su edad");{
-        if(edadIngresada >= 20){
-            alert("Sos mayor de 20 años, genial. Sigamos...");
-            let numeroIngresado = prompt("Elegí un numero del 1 al 10")
-            let resultado = numeroIngresado * 5
-                if (numeroIngresado > 5){
-                    alert("Lo vamos a multiplicar por 5");
-                    alert(resultado);
-                } else{
-                    alert("Lo vamos a dividir por "+numeroIngresado);
-                    alert("1");
-                }
-        } else{
-            alert("Esperaba que seas mayor jejej")
-        }
+
+alert("Bienvenidas y Bienvenidos");
+let bienvenidxs = prompt("¿Que día es hoy?");
+alert("Genial, hoy es " + bienvenidxs);
+
+let usuario = prompt("Ingrese su nombre: ");
+let contraseña = prompt("Ingrese una clave: ")
+alert("Nunca comparta su clave con nadie, menos con nosotros")
+alert("Le indicaremos un número de turno para que podamos coordinar y asesorarlo")
+    for(i = 1; i <= 100; i++){
+        alert("Su turno es el: " + i + ", a nombre de: " + usuario)
+    break
     }
-} else{
-    alert("No sos mi tutora");
-}
 
 console.log("FIN");
