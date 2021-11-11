31f9b0310441ea0ace1978faf898f97dc6c265f7
soto-manuel <manussoto69@gmail.com>
2021-11-10 21:12:45 -0300

    RE entrega del desafio - clase 3

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 console.log("INICIO");
 
 
-
-
 alert("Bienvenidas y Bienvenidos");
 let bienvenidxs = prompt("¿Que día es hoy?");
 alert("Genial, hoy es " + bienvenidxs);
 
 let usuario = prompt("Ingrese su nombre: ");
-let contraseña = prompt("Ingrese una clave: ")
-alert("Nunca comparta su clave con nadie, menos con nosotros")
-alert("Le indicaremos un número de turno para que podamos coordinar y asesorarlo")
-    for(i = 1; i <= 100; i++){
-        alert("Su turno es el: " + i + ", a nombre de: " + usuario)
-    break
-    }
+let contraseñaIngresada = prompt("Ingrese su clave: ");
+let contraseñaReingresada = prompt("Vuelva a ingresar su clave");
+
+while (contraseñaReingresada !== contraseñaIngresada){
+    contraseñaReingresada = prompt("Contraseña incorrecta, intente nuevamente");
+}
+alert("Contraseña valida");
+
+
+alert("Nunca comparta su clave con nadie, menos con nosotros");
+
 
 console.log("FIN");
