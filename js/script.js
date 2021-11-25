099760aabdbe11e49953a192357504bfde88cdaf
soto-manuel <manussoto69@gmail.com>
2021-11-25 18:57:08 -0300

    re entrega del proyecto final

--- js/script.js
+++ js/script.js
@@ -8,18 +8,25 @@ function bienvenida() {
 
 
 function ingresarDatos() {
-    let usuario = prompt("Ingrese su nombre: ");
-    console.log(usuario);
-    let contraseñaIngresada = prompt("Ingrese su clave: ");
-    console.log(contraseñaIngresada);
-    let contraseñaReingresada = prompt("Vuelva a ingresar su clave");
-    console.log(contraseñaReingresada);
+    let usuario = prompt ("Ingrese su nombre: ")
 
-    while (contraseñaReingresada !== contraseñaIngresada){
-        contraseñaReingresada = prompt("Contraseña incorrecta, intente nuevamente");
-        console.log(contraseñaReingresada);
+    while (usuario === ""){
+        usuario = prompt("Ingrese un nombre valido")
     }
-    alert("Contraseña valida");
+
+    let contraseñaIngresada = prompt("Ingrese una clave: ")
+
+    while (contraseñaIngresada === "") {
+        contraseñaIngresada = prompt("Ingrese una clave valida ")
+    } 
+    let contraseñaReIngresada = prompt("Vuelva a ingresar su clave: ") 
+
+    while (contraseñaReIngresada !== contraseñaIngresada){
+        contraseñaReIngresada = prompt("Contraseña incorrecta, intente nuevamente")
+
+    }
+        alert("Contraseña valida");
+
 }
 
 function alerta() {
@@ -35,42 +42,57 @@ alerta()
 
 
 
-class Libros {
-    constructor(nombre, genero, editorial, autor, precio){
-        this.nombre = nombre;
-        this.genero = genero;
-        this.editorial = editorial.toUpperCase();
-        this.autor = autor;
-        this.precio = precio;
-        this.stock = 5;
-    }
-    vendido(){
-        this.vendido = "Lo sentimos pero no contamos con stock";
-    }
-}
+// class Libros {
+//     constructor(nombre, genero, editorial, autor, precio){
+//         this.nombre = nombre;
+//         this.genero = genero;
+//         this.editorial = editorial.toUpperCase();
+//         this.autor = autor;
+//         this.precio = precio;
+//         this.stock = 5;
+//     }
+//     vendido(){
+//         this.vendido = "Lo sentimos pero no contamos con stock";
+//     }
+// }
 
-const productos = []
+// const productos = []
 
-const libroUno = new Libros ("Peter Pan", "Fantástico", "Terramar", "James M. Barrie", 599)
-productos.push(libroUno);
-const libroDos = new Libros ("El Sabueso de los Baskerville", "Policiales", "Terramar", "Arthur Conan Doyle", 799)
-productos.push(libroDos);
-const libroTres = new Libros ("Las aventuras de Tom Sawyer", "Novela infantil", "Estrada", "versión de Nicolas Schuff", 299)
-productos.push(libroTres);
-const libroCuatro = new Libros ("Harry Potter y la Piedra Filosofal", "Fantástico", "Salamandra", "Rowling J.K.", 1999)
-productos.push(libroCuatro);
-const libroCinco = new Libros ("El Libro de los Dinosaurios", "Educativo", "Jerez Editores & DK", "John Woodward", 1999)
-
-
-libroTres.vendido;
-libroUno.vendido;
+// const libroUno = new Libros ("Peter Pan", "Fantástico", "Terramar", "James M. Barrie", 599)
+// productos.push(libroUno);
+// const libroDos = new Libros ("El Sabueso de los Baskerville", "Policiales", "Terramar", "Arthur Conan Doyle", 799)
+// productos.push(libroDos);
+// const libroTres = new Libros ("Las aventuras de Tom Sawyer", "Novela infantil", "Estrada", "versión de Nicolas Schuff", 299)
+// productos.push(libroTres);
+// const libroCuatro = new Libros ("Harry Potter y la Piedra Filosofal", "Fantástico", "Salamandra", "Rowling J.K.", 1999)
+// productos.push(libroCuatro);
+// const libroCinco = new Libros ("El Libro de los Dinosaurios", "Educativo", "Jerez Editores & DK", "John Woodward", 1999)
 
 
-let caretes = productos.filter((b) => b.precio > 600);
-console.log(caretes);
+// libroTres.vendido;
+// libroUno.vendido;
 
-let generoFantastico = productos.filter((f) => f.genero === "Fantástico");
-console.log(generoFantastico);
+
+// let caretes = productos.filter((b) => b.precio > 600);
+// console.log(caretes);
+
+// let generoFantastico = productos.filter((f) => f.genero === "Fantástico");
+// console.log(generoFantastico);
+
+
+
+
+
+
+// let boton = document.getElementById("btn-section");
+
+// boton.onclick = () => {
+//     console.log("aprete el boton")
+// }
+
+// let btnClean = document.getElementById("btn-clean")
+
+
 
 
 console.log("FIN");
